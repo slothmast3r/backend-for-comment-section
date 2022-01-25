ef23e1f3e1032655a2be478ba1a8261f82679a19
slothmast3r <oskar.straszynski@gmail.com>
2022-01-25 21:34:21 +0100

    cors

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express")
 
 const app = express();
 const importData =require("./data.json")
+const cors = require("cors");
 let port = process.env.PORT || 3000;
 
 app.get("/",((req,res)=>{
@@ -11,7 +12,7 @@ app.get("/",((req,res)=>{
 app.get("/data", (req,res)=>{
     res.send(importData)
 })
-
+app.use(cors());
 app.listen(port,()=>{
     console.log(`Exapmle app ${port}`)
 })
