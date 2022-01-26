acdd3027b027570ad3cda5b1ca970589656abce3
slothmast3r <oskar.straszynski@gmail.com>
2022-01-26 13:17:36 +0100

    postressql

--- index.js
+++ index.js
@@ -29,6 +29,20 @@ app.use((req, res, next) => {
 })
 
 
+
+app.get('/db', async (req, res) => {
+    try {
+        const client = await pool.connect();
+        const result = await client.query('SELECT * FROM test_table');
+        const results = { 'results': (result) ? result.rows : null};
+        res.render('pages/db', results );
+        client.release();
+    } catch (err) {
+        console.error(err);
+        res.send("Error " + err);
+    }
+})
+
 app.get("/",((req,res)=>{
     return res.status(200).json(importData)
 }))
